88c3bcd0c1b04dfee43321591b8f6557330384b2
6f8l <atsuki02jojo@gmail.com>
2019-08-14 14:17:44 +0900

    Change from argument to arrow function

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 class TodoItems extends Component {
-  createTasks(item) {
+  createTasks = item => {
     return <li key={item.key} onClick={() => this.props.deleteItem(item.key)}>
             {item.text}
            </li>
