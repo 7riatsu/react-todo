5ec6ad384782052c8faa8fd027c3cc4c34e43b23
6f8l <atsuki02jojo@gmail.com>
2019-08-14 14:16:58 +0900

    Create delete item function

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TodoList from './TodoList';
 import TodoItems from './TodoItems';
 
 class App extends Component {
+  inputElement = React.createRef()
   constructor() {
     super()
     this.state={
@@ -30,7 +31,6 @@ class App extends Component {
     e.preventDefault()
     const newItem = this.state.currentItem
     if (newItem.text !== '') {
-      console.log(newItem)
       const items = [...this.state.items, newItem]
       this.setState({
         items: items,
@@ -40,6 +40,15 @@ class App extends Component {
         }
       })
     }
+  }
+
+  deleteItem = key => {
+    const filteredItems = this.state.items.filter(item => {
+      return item.key !== key
+    })
+    this.setState({
+      items: filteredItems
+    })
   }
 
   render() {
